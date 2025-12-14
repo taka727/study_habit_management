970ffa1ba7d43cc522111f7292615a8f09618ac1
taka727 <taka_10727@icloud.com>
2025-12-14 16:20:17 +0900

    feat : APIを実装

--- backend/controller/booksController.js
+++ backend/controller/booksController.js
@@ -5,14 +5,14 @@ const getAllBooks = async (req, res) => {
   try {
     console.log("start");
     const book = await prisma.book_managements.findMany({
-        select:{
-            id:true,
-            title:true,
-            desicription:true
-        }
+      select: {
+        id: true,
+        title: true,
+        desicription: true,
+      },
     });
     console.log(book);
-    res.status(200).json({status:'success',book});
+    res.status(200).json({ status: "success", data: book });
   } catch (error) {
     res
       .status(500)
@@ -20,9 +20,62 @@ const getAllBooks = async (req, res) => {
   }
 };
 
-const getBookById = async (req, res) => {}; 
-const createBook = async (req, res) => {};
-const updateBook = async (req, res) => {};
+const getBookById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const book = await prisma.book_managements.findUnique({
+      where: { id: parseInt(id) },
+      select: {
+        id: true,
+        title: true,
+        desicription: true,
+      },
+    });
+    res.status(200).json({ status: "success", data: book });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ status: "error", message: "サーバーエラー:" + error.message });
+  }
+};
+const createBook = async (req, res) => {
+  const { title, desicription } = req.body;
+  try {
+    console.log("title：" + title);
+    console.log("desicription：" + desicription);
+    const book = await prisma.book_managements.create({
+      data: {
+        title: title,
+        desicription: desicription,
+      },
+    });
+    console.log(book);
+    res.status(201).json({status:"success",book});
+  } catch (error) {
+    res
+      .status(500)
+      .json({ status: "error", message: "サーバーエラー:" + error.message });
+  }
+};
+const updateBook = async (req, res) => {
+  const { id } = req.params;
+  const { title, desicription } = req.body;
+  try {
+    const book = await prisma.book_managements.updata({
+      where: { id: parseInt(id) },
+      data: {
+        title: title,
+        desicription: desicription,
+        updated_at: new Date(),
+      },
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ status: "error", message: "サーバーエラー:" + error.message });
+  }
+};
 const deleteBook = async (req, res) => {};
 
 module.exports = {
