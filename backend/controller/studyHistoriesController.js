32ef182c46f27a35c297a18f0450b8e49138812d
zikka_takahiro <taka132juggle@gmail.com>
2025-12-31 15:30:09 +0900

    feat : APIの処理部分を実装

--- backend/controller/studyHistoriesController.js
+++ backend/controller/studyHistoriesController.js
@@ -31,48 +31,117 @@ const getAllHistories = async (req, res) => {
 
 const createHistory = async (req, res) => {
   console.log("start createHistory");
+  const {task_id, description,occurreed_on,started_at,ended_at} = req.body;
   try {
-  } catch {
+    const newHistory = await prisma.study_histories.create({
+        data:{
+            task_id,
+            description,
+            occurreed_on,
+            started_at,
+            ended_at
+        }
+    });
+    res.status(201).json({status:'success',data:newHistory});
+  } catch (error) {
+    res.status(500).json({status:'error',message:error.message});
   } finally {
     console.log("end createHistory");
   }
 };
 const getHistory = async (req, res) => {
   console.log("start getHistory");
+  const { id } = req.params;
+
   try {
-  } catch {
+    const history = await prisma.study_histories.findUnique({
+        where:{
+            id:parseInt(id)
+        }
+    });
+    res.status(200).json({status:'success',data:history});
+  } catch (error) {
+    res.status(500).json({status:'error',message:error.message});
   } finally {
     console.log("end getHistory");
   }
 };
 const updateHistory = async (req, res) => {
   console.log("start updateHistory");
+  const { id } = req.params;
+  const {task_id, description,occurreed_on,started_at,ended_at} = req.body;
+
   try {
-  } catch {
+    const updatedHistory = await prisma.study_histories.update({
+        where:{
+            id:parseInt(id) 
+        },
+        data:{
+            task_id,
+            description,
+            occurreed_on,
+            started_at,
+            ended_at
+        }
+    });
+    res.status(200).json({status:'success',data:updatedHistory});
+  } catch (error) {
+    res.status(500).json({status:'error',message:error.message});
   } finally {
     console.log("end updateHistory");
   }
 };
 const deleteHistory = async (req, res) => {
+  const { id } = req.params;
   console.log("start deleteHistory");
   try {
-  } catch {
+    const deletedHistory = await prisma.study_histories.delete({  
+        where:{
+            id:parseInt(id)
+        }
+    });
+    res.status(200).json({status:'success',data:deletedHistory});
+  } catch (error) {
+    res.status(500).json({status:'error',message:error.message});
   } finally {
     console.log("end deleteHistory");
   }
 };
 const startStudyHistory = async (req, res) => {
   console.log("start startStudyHistory");
+  const { task_id, description, occurreed_on } = req.body;
   try {
-  } catch {
+    const newHistory = await prisma.study_histories.create({
+        data:{
+            task_id,
+            description,
+            occurreed_on,
+            started_at: new Date(),
+            ended_at: null
+        }
+    });
+    res.status(201).json({status:'success',data:newHistory});
+  } catch (error) {
+    res.status(500).json({status:'error',message:error.message});
   } finally {
     console.log("end startStudyHistory");
   }
 };
 const endStudyHistory = async (req, res) => {
   console.log("start endStudyHistory");
+  const { id } = req.params;
   try {
-  } catch {
+    const endedHistory = await prisma.study_histories.update({
+        where:{
+            id:parseInt(id)
+        },
+        data:{
+            ended_at: new Date()
+        }
+    });
+    res.status(200).json({status:'success',data:endedHistory});
+  } catch (error) {
+    res.status(500).json({status:'error',message:error.message});
   } finally {
     console.log("end endStudyHistory");
   }
