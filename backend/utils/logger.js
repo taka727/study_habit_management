66374d19c4874e3df7e0cd72ced63bc81d90c4b5
taka727 <taka_10727@icloud.com>
2026-01-22 21:08:25 +0900

    feat :loggerファイルの作成

---
+++ backend/utils/logger.js
@@ -0,0 +1,12 @@
+const pino =require('pino');
+
+const logger = pino({
+    level:"info",
+    transport:{
+        target:'pino-pretty',
+        options:{
+            colorize:true,
+            translateTime:''
+        }
+    }
+})
