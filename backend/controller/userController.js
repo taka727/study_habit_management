f50843776ca55f7d2961d9a6a39e5b72f15ace31
taka727 <taka_10727@icloud.com>
2026-01-22 21:57:54 +0900

    feat : loggerを反映

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,32 +1,33 @@
 const prisma = require("../prismaClient");
+const logger = require('./utils/logger');
 
 const getUser = async (req, res) => {
-  console.log("start getUser");
+  logger.info("start getUser");
   try {
   } catch (error){
     res.status(500).json({status:'error',message:error.message});
   } finally {
-    console.log("end getUser");
+    logger.info("end getUser");
   }
 };
 
 const updateUser = async (req, res) => {
-  console.log("start getUser");
+  logger.info("start getUser");
   try {
   } catch (error){
     res.status(500).json({status:'error',message:error.message});
   } finally {
-    console.log("end getUser");
+    logger.info("end getUser");
   }
 };
 
 const deleteUser = async (req, res) => {
-  console.log("start getUser");
+  logger.info("start getUser");
   try {
   } catch (error){
     res.status(500).json({status:'error',message:error.message});
   } finally {
-    console.log("end getUser");
+    logger.info("end getUser");
   }
 };
 
