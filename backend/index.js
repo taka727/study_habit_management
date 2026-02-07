5bd4a815492ce5925938799ba6b6bdfb7e6fd129
taka727 <taka_10727@icloud.com>
2026-02-07 15:18:50 +0900

    corsの設定を修正

--- backend/index.js
+++ backend/index.js
@@ -20,10 +20,9 @@ const prisma = require('./prismaClient');
 
 // CORS設定
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGIN || 'http://localhost:3000');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Content-Type', 'application/json; charset=utf-8');
     next();
 });
 
